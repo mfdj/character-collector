4fa11962c50af600c3b8a5141f14ed50fa79ca6a
Mark Fox <mf@ixel.org>
2017-10-05 11:50:52 -0700

    remove needless assignment

--- src/handle-post-middleware.js
+++ src/handle-post-middleware.js
@@ -1,9 +1,7 @@
 
 module.exports = function (req, res, next) {
-	let body = req.body
-
   req.urls = []
-	if (body && typeof body === 'string') {
+	if (req.body && typeof req.body === 'string') {
 		req.urls = req.body.split(',')
 	}
 
